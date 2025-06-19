5d545408d1a767841b29829ded9a5a2db17c374f
eva1606 <168004475+eva1606@users.noreply.github.com>
2025-06-19 11:39:52 +0300

    createCard function to generate item cards dynamically

--- js/app.js
+++ js/app.js
@@ -6,3 +6,23 @@ const themeToggle = document.getElementById('themeToggle');
 const themeIcon = document.getElementById('themeIcon');
 
 let allItems = []
+
+function createCard(item) {
+    const col = document.createElement('div');
+    col.className = 'col';
+  
+    col.innerHTML = `
+      <div class="card card-custom h-100">
+        <div class="card-img-container">
+          <img src="${item.image}" alt="${item.title}" />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+          <h6 class="card-subtitle mb-2 badge badge-author">${item.author}</h6>
+          <p class="card-text">${item.description}</p>
+        </div>
+      </div>
+    `;
+  
+    return col;
+  }
