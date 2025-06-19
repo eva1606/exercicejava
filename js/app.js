bd82f9a140b1bf19abb0065e258a3429d7d3af19
eva1606 <168004475+eva1606@users.noreply.github.com>
2025-06-19 11:38:13 +0300

    DOM elements and setup initial JavaScript variables

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const cardsContainer = document.getElementById('cardsContainer');
+const errorContainer = document.getElementById('errorContainer');
+const searchInput = document.getElementById('searchInput');
+const sortSelect = document.getElementById('sortSelect');
+const themeToggle = document.getElementById('themeToggle');
+const themeIcon = document.getElementById('themeIcon');
+
+let allItems = []
